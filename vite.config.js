5e1538d89d072b5bd936071672777484e1961392
eslamibrahim30 <eslamelbhnaswee110@gmail.com>
2026-03-25 15:43:14 +0200

    fix: vite config file path bug

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
 	base: process.env.VITE_BASE || '/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
-			'@': new URL('./src', import.meta.url).pathname,
+			'@': path.resolve(__dirname, './src'),
 		},
 	},
 });
